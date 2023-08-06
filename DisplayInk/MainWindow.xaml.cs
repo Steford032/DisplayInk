104fbfbcd1fe77855b008cec5f1995b587a931d1
Steford032 <prudniko0356@gmail.com>
2023-08-06 05:14:35 +0300

    Класс ArrayPoints вынесен, как отдельныйй

--- DisplayInk/MainWindow.xaml.cs
+++ DisplayInk/MainWindow.xaml.cs
@@ -26,40 +26,6 @@ namespace DisplayInk
 		private bool isPaint = false;
 		private bool isErase = false;
 
-		private class ArrayPoints
-		{
-			private int index = 0;
-			private Point[] points;
-            
-            public ArrayPoints(int size)
-			{
-				if (size <= 0) size = 2;
-				points = new Point[size];
-            }
-
-			public void SetPoint(int x, int y)
-			{
-				if (index >= points.Length) index = 0;
-				points[index] = new Point(x, y);
-				index++;
-			}
-
-			public void Reset()
-			{
-				index = 0;
-			}
-
-			public int Index()
-			{
-				return index;
-			}
-			public Point[] getPoints()
-			{
-				return points;
-			}
-
-		}
-
 		private ArrayPoints points = new ArrayPoints(2);
 
 		public MainWindow()
